b728e68977a2190a914c7afc5fa4ea03ae7eaf1f
Dmytro Rebenko <dima.rebenko@gmail.com>
2021-03-02 11:02:44 +0200

    Initial Commit

---
+++ AsyncTesting/AsyncTesting.h
@@ -0,0 +1,18 @@
+//
+//  AsyncTesting.h
+//  AsyncTesting
+//
+//  Created by AndAdmin on 02.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsyncTesting.
+FOUNDATION_EXPORT double AsyncTestingVersionNumber;
+
+//! Project version string for AsyncTesting.
+FOUNDATION_EXPORT const unsigned char AsyncTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncTesting/PublicHeader.h>
+
+
